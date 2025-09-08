66237d7385e2bf2ae62f772fa397f190a351da2b
Jorge Cristo <dev.jorge.c@gmail.com>
2025-09-07 23:28:55 -0300

    Delete .next directory

--- .next/static/chunks/src_app_login_page_tsx_d617e48f._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@supabase_node-fetch_browser_b98b7084.js",
-  "static/chunks/node_modules_8a021e5a._.js",
-  "static/chunks/src_471cbc4f._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_page_tsx_d617e48f._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@supabase_node-fetch_browser_139f4b48.js",
-  "static/chunks/src_18e50665._.js",
-  "static/chunks/node_modules_7f9540bc._.js"
-],
-    source: "dynamic"
-});
